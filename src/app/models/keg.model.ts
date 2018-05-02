6649130d464bc2e5c012f6e8b80f1f8edbb5a971
Garnett Puett and William Chu <garnett@>
2018-05-02 09:16:20 -0700

    Add sell pint method.

--- src/app/models/keg.model.ts
+++ src/app/models/keg.model.ts
@@ -1,4 +1,8 @@
 export class Keg {
   pints: number = 124;
   constructor(public price: number, public name: string, public brand: string, public alcoholContent: number){ };
+
+  sellPint() {
+    this.pints -= 1;
+  }
 }
